b98e076f5eb266dfe20eccf96beeaeb38f05a318
Codin-Maestro <dimahtee@gmail.com>
2022-10-18 14:45:47 +0100

    Corrected for Betty

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -3,15 +3,18 @@
 
 
 /**
- * main - check the code
+ * print_alphabet - prints the alphabet in lowercase
  *
  * Return: Always 0.
  */
 
 void print_alphabet(void)
 {
-    char a;
-	for(a = 'a'; a <= 'z'; a++)
+	char a;
+
+	for (a = 'a'; a <= 'z'; a++)
+{
 		printf("%c", a);
+}
 	printf("\n");
 }
